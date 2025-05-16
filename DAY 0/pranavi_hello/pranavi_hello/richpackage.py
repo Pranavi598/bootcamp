1c3a8378d257fd8561b6a209775419c2a9e67edf
Pranavik <pranavik928@gmail.com>
2025-05-16 19:21:21 +0530

    Delete DAY 0/pranavi_hello directory

--- DAY 0/pranavi_hello/pranavi_hello/richpackage.py
+++
@@ -1,8 +0,0 @@
-from rich.console import Console
-
-console = Console()
-console.print("[bold green]Hello! This message is printed using Rich.[/bold green]")
-console.print("You can also display [underline magenta]styled text[/underline magenta], emojis :smile:, and more!")
-
-
-

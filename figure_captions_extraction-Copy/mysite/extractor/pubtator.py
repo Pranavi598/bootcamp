4975dd481ec7ed73e71e6c4dc37de095ce8616b3
Pranavik <pranavik928@gmail.com>
2025-05-19 19:40:32 +0530

    Update pubtator.py

--- figure_captions_extraction-Copy/mysite/extractor/pubtator.py
+++ figure_captions_extraction-Copy/mysite/extractor/pubtator.py
@@ -31,38 +31,5 @@ def extract_entities_from_text(text: str) -> List[Dict]:
         })
 
     return entities
-#import requests
-#from typing import List, Dict
-
-# def extract_entities_from_pmid(pmid: str) -> List[Dict]:
-#     url = f"https://www.ncbi.nlm.nih.gov/research/pubtator-api/annotations/pmids/{pmid}"
-#     headers = {
-#         "Accept": "application/json"
-#     }
-#
-#     response = requests.get(url, headers=headers)
-#
-#     if response.status_code == 404:
-#         return []  # No entities found
-#     elif response.status_code != 200:
-#         raise ValueError(f"Failed to extract entities: {response.status_code} -> {response.text}")
-#
-#     data = response.json()
-#
-#     entities = []
-#     for passage in data.get("passages", []):
-#         for ann in passage.get("annotations", []):
-#             start = ann.get("locations", [{}])[0].get("offset")
-#             length = ann.get("locations", [{}])[0].get("length")
-#             text = passage.get("text", "")[start : start + length]
-#             entities.append({
-#                 "id": ann.get("id"),
-#                 "type": ann.get("infons", {}).get("type"),
-#                 "start": start,
-#                 "length": length,
-#                 "text": text
-#             })
-#
-#     return entities
 
 
